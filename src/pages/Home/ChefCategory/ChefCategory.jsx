b1ac4517fc39be17992a834e5399587452d63f09
Pranto-Nondie <pranto.cs.25@gmail.com>
2023-05-02 03:22:06 +0600

    progress

---
+++ src/pages/Home/ChefCategory/ChefCategory.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ChefCategory = () => {
+    return (
+        <div>
+            this is chef category
+        </div>
+    );
+};
+
+export default ChefCategory;
