f7ecd602bbbaca500f746bfa1f6d20d6403d8e9e
Pranto-Nondie <pranto.cs.25@gmail.com>
2023-05-02 18:16:46 +0600

    card upaded of chef category

--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
@@ -10,7 +11,7 @@ const Header = () => {
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </label>
                         <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52 text-xl ">
-                            <li><a>Home</a></li>
+                        <Link to='/' > <li><a>Home</a></li></Link>
                             <li><a>Our Chef</a></li>
                             <li><a>Recipes</a></li>
                             <li><a>Blog</a></li>
@@ -20,13 +21,13 @@ const Header = () => {
                 </div>
                 <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal px-1 text-2xl">
-                        <li><a>Home</a></li>
+                        <Link to='/' > <li><a>Home</a></li></Link>
                         <li><a>Our Chef</a></li>
                         <li><a>Recipes</a></li>
                         <li><a>Blog</a></li>
                     </ul>
                 </div>
-               
+
                 <div className="navbar-end">
                     <div className="flex items-center space-x-4">
                         <a className="btn text-sm md:text-lg lg:text-lg">Login</a>
