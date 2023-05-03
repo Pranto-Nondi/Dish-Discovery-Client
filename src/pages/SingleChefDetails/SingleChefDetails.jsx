17ad77ebfb340adb0d32a44f4aecc50ca4d57635
Pranto-Nondie <pranto.cs.25@gmail.com>
2023-05-03 13:17:34 +0600

    wokring on it

--- src/pages/SingleChefDetails/SingleChefDetails.jsx
+++ src/pages/SingleChefDetails/SingleChefDetails.jsx
@@ -1,8 +1,17 @@
-import React from 'react';
-import { FaThumbsUp } from 'react-icons/fa';
+import React, { useState } from 'react';
+import { Rating } from '@smastrom/react-rating'
+import '@smastrom/react-rating/style.css'
 import { Link } from 'react-router-dom';
 const SingleChefDetails = ({ recipe }) => {
     const { recipeName, ingredients, cookingMethod, rating } = recipe
+    const [showFullMethod, setShowFullMethod] = useState(false);
+
+    const methodPreview = cookingMethod.slice(0, 50);
+    const methodFull = cookingMethod.slice(50);
+
+    const toggleShowFullMethod = () => {
+        setShowFullMethod(!showFullMethod);
+    };
     return (
         <div>
             <div className="card w-96 card-compact bg-base-100 shadow-xl">
@@ -12,12 +21,26 @@ const SingleChefDetails = ({ recipe }) => {
                 </figure>
                 <div className="card-body">
                     <h2 className="card-title text-2xl ">{recipeName}</h2>
-                    <p className='text-xl '>Experience: yrs </p>
-                    <p className='text-lg '>Recipe Items :  </p>
-                    {/* <p className='text-sm font-semibold'>likes people :{likes} </p> */}
+                    <p className='text-xl font-semibold '>Ingredients </p>
+                    <ul className=" list-disc list-inside">
+                        {ingredients.map((ingredient, index) => (
+                            <li key={index} className="my-2 text-md font-semibold">
+                                {ingredient}
+                            </li>
+                        ))}
+                    </ul>
+                   
+                    <p className="text-lg">
+                        <span className='font-semibold'>Cooking Method </span>:{" "}
+                        {showFullMethod ? cookingMethod : methodPreview + "..."}
+                        <button onClick={toggleShowFullMethod} className="text-red-500 hover:text-red-700">
+                            {showFullMethod ? "Read Less" : "Read More"}
+                        </button>
+                    </p>
                     <div className=' flex justify-center align-bottom'>
-                        <FaThumbsUp className='text-blue-600 text-2xl'></FaThumbsUp>
-                        <p className='text-xl pb-1'>&nbsp;&nbsp; </p>
+                        <Rating style={{ maxWidth: 140 }} value={(rating)} readOnly />
+
+                        <p className='text-xl pb-1'>&nbsp;&nbsp;{rating} </p>
                     </div>
                     <div className="card-actions justify-center">
                         <Link className="btn btn-primary">Favorite</Link>
